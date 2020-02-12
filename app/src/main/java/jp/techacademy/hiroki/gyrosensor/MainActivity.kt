b75d048ff8e637360625ea9d97c7842400c80967
TaniseHiroki <tanise@scsk.jp>
2020-02-12 14:19:33 +0900

    ジャイロセンサー単体完成版

--- app/src/main/java/jp/techacademy/hiroki/gyrosensor/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiroki/gyrosensor/MainActivity.kt
@@ -1,19 +1,29 @@
 package jp.techacademy.hiroki.gyrosensor
 
 
+import android.content.Context
 import android.os.Bundle
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.TextView
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var sensorManager: SensorManager? = null
     private var textView: TextView? = null
     private var textInfo: TextView? = null
+    val FILENAME = "practicefile.txt"
+    var contents = "初期値"
+    private var suddenMoveTime:Long = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -28,6 +38,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         // Get an instance of the TextView
         textView = findViewById(R.id.text_view)
 
+        val readFile = File(applicationContext.filesDir, FILENAME)
+        if(readFile.exists()){
+            deleteFile(FILENAME)
+            Log.d("gyro_test","delete")
+        }
     }
 
     override fun onResume() {
@@ -59,6 +74,40 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             sensorX = event.values[0]
             sensorY = event.values[1]
             sensorZ = event.values[2]
+
+            if(getNowTime() > suddenMoveTime + 2000 ){ // 前回記録時から2000ミリ秒以上経過していたら
+                val date = Date()
+                val format = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault())
+
+                // 急停止/急加速時の処理
+                if(sensorZ >= 20) { // スマホの画面側に加速（急加速）
+                    try {
+                        suddenMoveTime = getNowTime() // 記録時間を更新
+                        val fos = openFileOutput(FILENAME, Context.MODE_APPEND)
+                        Log.d(
+                            "gyro_test", "Time: " + suddenMoveTime
+                                    + "\tsensorZ: " + sensorZ + " accele"
+                        )
+                        fos.write("${suddenMoveTime} ${sensorZ} acsele\n".toByteArray())
+                        fos.close()
+                    }catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                } else if (sensorZ <= -20){ // スマホの背面側に加速（急停止）
+                    try {
+                        val fos = openFileOutput(FILENAME, Context.MODE_APPEND)
+                        suddenMoveTime = getNowTime() // 記録時間を更新
+                        Log.d(
+                            "gyro_test", "Time: " + suddenMoveTime
+                                    + "\tsensorZ: " + sensorZ + " brake"
+                        )
+                        fos.write("${suddenMoveTime} ${sensorZ} brake\n".toByteArray())
+                        fos.close()
+                    }catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
 
             val strTmp = ("加速度センサー\n"
                     + " X: " + sensorX + "\n"
@@ -136,5 +185,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
 
+    }
+
+    // 1970 年 1 月 1 日 00:00:00から現在までの経過時間をミリ秒で表した数値を返す関数
+    private fun getNowTime(): Long{
+        val date = Date()
+        return date.time
     }
 }
